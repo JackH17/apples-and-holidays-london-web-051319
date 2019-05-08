0f9b60ccad804017c5def3a852111efa69e6cefe
Jack Hawkins <jack.hawkins77@googlemail.com>
2019-05-08 09:27:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,6 +48,26 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  
+   {
+:winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]},
+:summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]},
+:fall => {
+    :thanksgiving => ["Turkey"]},
+:spring => {
+  :memorial_day => ["BBQ"]}
+}
+  # return the second element in the 4th of July array
+  
+ holiday_hash[:spring].each do |holiday, decorations|
+  
+  decorations << supply 
+  
+  
+  
 
 end
 
